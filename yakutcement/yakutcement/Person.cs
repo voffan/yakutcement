676d6497a82a2d6c482f6381fbb8e37055a7cfd1
voffan <vovan_evv@rambler.ru>
2020-03-25 16:12:29 +0900

    Added some diagrams and Project files

---
+++ yakutcement/yakutcement/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace yakutcement
+{
+    public enum Position { Manager, Keeper, Admin, PlantMan, QuarryMan }
+    public enum Level { Manager, Keeper, Admin, PlantMan, QuarryMan, Client }
+
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public String SecondName { get; set; }
+        public String LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Position Position { get; set; }
+        public double Salary { get; set; }
+        public Level Level { get; set; }
+    }
+}
---
+++ yakutcement/yakutcement/Quarry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace yakutcement
+{
+    public class Quarry
+    {
+        [Key]
+        public int Id { get; set; }
+        public int MasterId { get; set; }
+        [ForeignKey("MasterId")]
+        public virtual Person Master { get; set; }
+    }
+}
