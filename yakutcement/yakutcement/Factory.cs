b293b3cbfae643d36741cbad00c3dfcac833a8a3
Trinkeen <vadim.budischev@gmail.com>
2020-03-26 02:51:37 +0900

    Yes

--- yakutcement/yakutcement/Factory.cs
+++ yakutcement/yakutcement/Factory.cs
@@ -5,19 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
 namespace yakutcement
 {
     public class Factory
     {
         [Key]
-        public int Id {get;set;}
-        public int MasterId {get;set;}
+        public int Id { get; set; }
+        public int MasterId { get; set; }
         [ForeignKey("MasterId")]
-        public virtual Person Master {get;set;}
-        public int PlantId {get;set;}
+        public virtual Person Master { get; set; }
+        public int PlantId { get; set; }
         [ForeignKey("PlantId")]
-        public virtual Plant Plant {get;set;}
-        public virtual ICollection<Productionline> ProductionLines {get;set;}
+        public virtual Plant Plant { get; set; }
+        public virtual ICollection<ProductionLine> ProductionLines { get; set; }
     }
 }
