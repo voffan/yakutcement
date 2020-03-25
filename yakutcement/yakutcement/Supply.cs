ede754fa02ba6db07a2c8968768e157d9d1ce5cd
StreetBrawler <nsn7.nshepard83@gmail.com>
2020-03-25 22:56:13 +0900

    124

---
+++ yakutcement/yakutcement/Supply.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace yakutcement
+{
+	public enum SupplyType { Raw, Processed }
+	
+    public class Supply
+    {
+		
+        [Key]
+        public int SuppId { get; set; }
+		[ForeignKey("SuppId")]
+        public string Name { get; set; }
+		public string Info { get; set; }
+		public double Price { get; set; }
+		public virtual SupplyType SupplyType { get; set; }
+		
+    }
+}
