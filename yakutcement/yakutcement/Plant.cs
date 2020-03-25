2adea08be728e9cbd83eaac13fc06bf427e9d3c3
voffan <vovan_evv@rambler.ru>
2020-03-25 16:22:21 +0900

    Added Plant class

--- yakutcement/yakutcement/Plant.cs
+++ yakutcement/yakutcement/Plant.cs
@@ -8,15 +8,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace yakutcement
 {
-    public class Quarry
+    public class Plant
     {
         [Key]
         public int Id { get; set; }
-        public int MasterId { get; set; }
-        [ForeignKey("MasterId")]
-        public virtual Person Master { get; set; }
-        public int PlantId { get; set; }
-        [ForeignKey("PlantId")]
-        public virtual Plant Plant { get; set; }
+        // some other fields
+
+        public virtual ICollection<Quarry> Quarries { get; set; }
     }
 }
