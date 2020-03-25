9fb37498322ff4c348046a698a13e6b840524f68
Trinkeen <46102086+Trinkeen@users.noreply.github.com>
2020-03-26 02:33:52 +0900

    Add files via upload

---
+++ yakutcement/yakutcement/Factory.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace yakutcement
+{
+    public class Factory
+    {
+        [Key]
+        public int Id {get;set;}
+        public int MasterId {get;set;}
+        [ForeignKey("MasterId")]
+        public virtual Person Master {get;set;}
+        public int PlantId {get;set;}
+        [ForeignKey("PlantId")]
+        public virtual Plant Plant {get;set;}
+        public virtual ICollection<Productionline> ProductionLines {get;set;}
+    }
+}
