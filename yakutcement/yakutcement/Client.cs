53b4ad461c3973d69be7f281946929954d42fc4a
Leonid Imeev <leonidimeev@yandex.ru>
2020-03-26 12:12:14 +0900

    Fixed Client class

--- yakutcement/yakutcement/Client.cs
+++ yakutcement/yakutcement/Client.cs
@@ -9,20 +9,28 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace yakutcement
 {
   
-    //Физ лицо или юр лицо
-    public enum ClientType { Individual, Entity }
+    // Физ лицо или юр лицо
+    //public enum ClientType { Individual, Entity }
 
     public class Client
     {
         [Key]
         public int PersonId { get; set; }
         [ForeignKey("PersonId")]
-        public virtual Person Master { get; set; }
-        public ClientType Type { get; set; }
+        public virtual Person Client { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+        public string Inn { get; set; }
+        //public ClientType Type { get; set; }
     }
-    /* Если же м все таки добавим компании - клиенты:
-     * 
-     * public int CompanyId { get; set; }
-        [ForeignKey("CompanyId")]
+
+    /* Если же мы все таки добавим компании - клиенты:
+
+    public int CompanyId { get; set; }
+    [ForeignKey("CompanyId")]
+    public virtual Сompany Client { get; set; }
+    
      */
 }
