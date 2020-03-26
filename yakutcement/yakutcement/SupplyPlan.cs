aa6f43857d4832a85364c3eb2e30f05b6b5667dc
Obtopy <savvin19990607@gmail.com>
2020-03-26 21:47:03 +0900

    added SupplyPlan

---
+++ yakutcement/yakutcement/SupplyPlan.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace yakutcement
+{
+    public class SupplyPlan
+    {
+        [Key]
+        public int Id { get; set; }
+	public DateTime Date { get; set; }
+	public double Value { get; set; }
+	public string Progress { get; set; }
+        public int SupplyId { get; set; }
+        [ForeignKey("SupplyId")]
+        public virtual Supply Supply { get; set; }
+        public int MasterId{ get; set; }
+        [ForeignKey("MasterId")]
+        public virtual Person Master { get; set; }
+    }
+}
