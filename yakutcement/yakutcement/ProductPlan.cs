4b2e2a46de6bfcb55fa134fe92830e21d7fc7a37
Artrka <46183366+Artrka@users.noreply.github.com>
2020-04-09 00:28:34 +0900

    add ProductPlan.cs please check

---
+++ yakutcement/yakutcement/ProductPlan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace yakutcement
+{
+    //public enum Progress { start, inwork, done } ?
+
+    public class ProductPlan
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Value { get; set; }
+        [StringLength(200)]
+        public string Progress { get; set; }
+        //public Progress Progress { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+        [ForeignKey("MasterId")]
+        public virtual Person Master { get; set; }
+    }
+}
