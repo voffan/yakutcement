47c73cd8bf2c21ddc7b6bdf0b4e6352c6bfa5249
mnsov <mayaokon99@gmail.com>
2020-04-08 22:02:51 +0900

    add SupplyRemain.cs

---
+++ yakutcement/yakutcement/SupplyRemain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace yakutcement
+{
+    public class SupplyRemain
+    {
+        [Key]
+        public int Id { get; set; }
+        public int SupplyId { get; set; }
+        [ForeignKey("SupplyId")]
+        public virtual Supply Supply { get; set; }
+        public double Value { get; set; }
+    }
+}
