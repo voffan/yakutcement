a2478b40092f6b9a778279b19fad8add9e73b217
Sakhayaana <60932262+Sakhayaana@users.noreply.github.com>
2020-04-09 05:42:00 +0900

    Add Plant.cs

---
+++ Plant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace yakutcement
+{
+    public class Plant
+    {
+        [Key]
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string addres { get; set; }
+        public string telephone { get; set; }
+        public int inn { get; set; }
+        public int kpp { get; set; }
+        // some other fields
+
+        public virtual ICollection<Quarry> Quarries { get; set; }
+    }
+}
