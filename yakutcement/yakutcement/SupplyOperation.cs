2799e7d5b6c5d19897050a5bc2b455acbaa34842
fizzlefrost <37946205+fizzlefrost@users.noreply.github.com>
2020-04-09 15:03:16 +0900

    I guess done

--- yakutcement/yakutcement/SupplyOperation.cs
+++ yakutcement/yakutcement/SupplyOperation.cs
@@ -1,1 +1,25 @@
-placeholder
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace yakutcement
+{
+    public class SupplyOperation
+    {
+        [Key]
+        public int Id { get; set; }
+	      public DateTime Date { get; set; }
+	      public double Value { get; set; }
+	      public string OperationType { get; set; }
+        public int SupplyId { get; set; }
+        [ForeignKey("SupplyId")]
+        public virtual Supply Supply { get; set; }
+        public int MasterId{ get; set; }
+        [ForeignKey("MasterId")]
+        public virtual Person Master { get; set; }
+    }
+}
