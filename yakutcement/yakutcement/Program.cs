6b2de4c7605c82075b7be9a7b8619f78f180a641
voffan <vovan_evv@rambler.ru>
2020-04-15 15:36:26 +0900

    Added second window

--- yakutcement/yakutcement/Program.cs
+++ yakutcement/yakutcement/Program.cs
@@ -16,7 +16,7 @@ namespace yakutcement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
