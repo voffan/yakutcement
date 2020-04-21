0e3f0059fd6cb58e3e9130acebf2d32ea22dd162
Alekseev Nikita <rrrrsrrr123@users.noreply.github.com>
2020-04-21 19:07:38 +0900

    Added LineSpeed

---
+++ yakutcement/yakutcement/LineSpeed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace yakutcement
+{
+	public class LineSpeed
+	{
+		[Key]
+		public int Id { get; set; }
+		public double Speed { get; set; }
+	}
+}
