08a2a43c30370e880210d7b5b86a8b2537c33423
voffan <vovan_evv@rambler.ru>
2020-04-22 14:53:15 +0900

    Added ConnectionString

--- yakutcement/yakutcement/Context.cs
+++ yakutcement/yakutcement/Context.cs
@@ -8,14 +8,21 @@ namespace yakutcement
     public partial class DBContext : DbContext
     {
         public DBContext()
-            : base("")
+            : base("YakutCementCompact")
         {
         }
 
         public virtual DbSet<Client> Clients { get; set; }
+        public virtual DbSet<Factory> Factories { get; set; }
+        public virtual DbSet<Quarry> Quarries { get; set; }
+        public virtual DbSet<Person> Persons { get; set; }
+        public virtual DbSet<Supply> Supplies { get; set; }
+        public virtual DbSet<SupplyOperation> SupplyOperations { get; set; }
+        public virtual DbSet<SupplyRemain> SupplyRemains { get; set; }
 	    public virtual DbSet<Plant> Plants { get; set; }
 	    public virtual DbSet<ProductPlan> ProductPlans { get; set; }
-	    public virtual DbSet<ProductionLine> ProductionLines { get; set; }
+	    //public virtual DbSet<ProductionLine> ProductionLines { get; set; }
+        //public virtual DbSet<ProductRemain> ProductRemains { get; set; }
 	    public virtual DbSet<SupplyPlan> SupplyPlans { get; set; }
 	    public virtual DbSet<Wherehouse> Warehouses { get; set; }
         public virtual DbSet<ProductOperation> ProductOperations { get; set; }
