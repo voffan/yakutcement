da53147bbba87878bba21a2d880466832c3ffc2d
voffan <vovan_evv@rambler.ru>
2020-04-22 16:00:45 +0900

    Added class productionLine to Project

--- yakutcement/yakutcement/ProductionLine.cs
+++ yakutcement/yakutcement/ProductionLine.cs
@@ -7,16 +7,17 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace yakutcement
 {
+    public enum Status { working, broken }
+
 	public class ProductionLine
 	{
 		[Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 		[StringLength(300)]
-		public string Status { get; set; }
+		public Status Status { get; set; }
 		public int FactoryId { get; set; }
 		[ForeignKey("FactoryId")]
 		public virtual Factory Factory { get; set; }
-		public virtual ICollection<Product> Products { get; set; }
 	}
 }
