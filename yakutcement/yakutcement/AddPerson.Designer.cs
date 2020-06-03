74797c8fb8d971a1f42febfe14f47f77f4c07aa0
voffan <vovan_evv@rambler.ru>
2020-06-03 15:12:29 +0900

    Final Fixes

--- yakutcement/yakutcement/AddPerson.Designer.cs
+++ yakutcement/yakutcement/AddPerson.Designer.cs
@@ -52,14 +52,14 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(22, 34);
+            this.textBox1.Location = new System.Drawing.Point(22, 73);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(283, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(22, 73);
+            this.textBox2.Location = new System.Drawing.Point(21, 34);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(283, 20);
             this.textBox2.TabIndex = 1;
@@ -75,14 +75,14 @@
             // 
             this.dateTimePicker1.Location = new System.Drawing.Point(22, 151);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(143, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(282, 20);
             this.dateTimePicker1.TabIndex = 3;
             this.dateTimePicker1.Value = new System.DateTime(1991, 12, 26, 0, 0, 0, 0);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 18);
+            this.label1.Location = new System.Drawing.Point(19, 57);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(29, 13);
             this.label1.TabIndex = 4;
@@ -91,7 +91,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 57);
+            this.label2.Location = new System.Drawing.Point(18, 18);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 5;
@@ -120,9 +120,9 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(19, 188);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.Size = new System.Drawing.Size(65, 13);
             this.label5.TabIndex = 8;
-            this.label5.Text = "Позиция";
+            this.label5.Text = "Должность";
             // 
             // comboBox1
             // 
@@ -153,9 +153,9 @@
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(19, 267);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(33, 13);
+            this.label7.Size = new System.Drawing.Size(94, 13);
             this.label7.TabIndex = 12;
-            this.label7.Text = "Level";
+            this.label7.Text = "Уровень доступа";
             // 
             // comboBox2
             // 
@@ -194,6 +194,7 @@
             // 
             this.textBox6.Location = new System.Drawing.Point(21, 377);
             this.textBox6.Name = "textBox6";
+            this.textBox6.PasswordChar = '*';
             this.textBox6.Size = new System.Drawing.Size(283, 20);
             this.textBox6.TabIndex = 14;
             // 
